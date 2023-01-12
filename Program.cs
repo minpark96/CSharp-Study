48df1fd188abf3bcbb49e2101d0a058e2a172eaf
minpark96 <hmpark3355@gmail.com>
2023-01-12 23:49:42 +0900

    1. type

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            // 데이터 + 로직
+            // 체력 0
+
+            // byte(1바이트 0~255), short(2바이트 -3만~3만), int(4바이트 -21억~21억), long(8바이트)
+            // sbyte(1바이트, -128~127, ushort(2바이트, 0~6만, uint(4바이트 0~43억, ulong(8바이트)
+            int hp;
+            short level = 100;
+            long id;
+            hp = 100;
+
+            byte attack = 0;
+            attack--;
+
+            Console.WriteLine("Hello Number ! {0}", hp);
         }
     }
 }
