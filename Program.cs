26e398efc6b55663a23dc02199ff3c47f6a7eaea
minpark96 <hmpark3355@gmail.com>
2023-01-14 23:49:43 +0900

    2. binary, hex

--- Program.cs
+++ Program.cs
@@ -11,15 +11,29 @@ namespace CSharp
 
             // byte(1바이트 0~255), short(2바이트 -3만~3만), int(4바이트 -21억~21억), long(8바이트)
             // sbyte(1바이트, -128~127, ushort(2바이트, 0~6만, uint(4바이트 0~43억, ulong(8바이트)
-            int hp;
-            short level = 100;
-            long id;
-            hp = 100;
+            //int hp;
+            //short level = 100;
+            //long id;
+            //hp = 100;
 
-            byte attack = 0;
-            attack--;
+            //byte attack = 0;
+            //attack--;
 
-            Console.WriteLine("Hello Number ! {0}", hp);
+            // 10진수
+            // 0 1 2 3 4 5 6 7 8 9
+                // 10
+
+            // 2진수
+            // 0~1
+                // 0b00 0b01 0b10 0b11 0b100
+                // 0b10001111 = 0x8F
+
+            // 16진수
+            // 0~9 a b c d e f
+                // 0x00 0x01 0x02 .. 0x0F
+                    // 0x10
+
+            //Console.WriteLine("Hello Number ! {0}", hp);
         }
     }
 }
